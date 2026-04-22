25e0e4e9894d91096dfb9a8efea5ee359be0345c
kaungsike <kaungsike9@gmail.com>
2026-04-22 16:44:09 +0630

    feat: add home screen with staff component to index tab

--- 1-startup/app/(tabs)/index.tsx
+++ 1-startup/app/(tabs)/index.tsx
@@ -1,26 +1,33 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 type StaffProps = {
   name: string;
+  age?: number;
 };
 
-const Staff = ({ name }: StaffProps) => {
+const Staff = ({ name, age  }: StaffProps) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffffff' }}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#11181C' }}>
-        {name}
-      </Text>
-    </View>
+    <Text style={{ fontSize: 20, fontWeight: "bold", color: "#11181C" }}>
+      {name} {age? `- ${age}` : ""}
+    </Text>
   );
 };
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffffff' }}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#11181C' }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <Text style={{ fontSize: 25, fontWeight: "bold", color: "#11181C" }}>
         Hello Mobile Developer!
       </Text>
       <Staff name="John Doe" />
+      <Staff name="Smith" age={21} />
     </View>
   );
 }
