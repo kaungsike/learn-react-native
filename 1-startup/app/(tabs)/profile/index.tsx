5d4347554a6d0e75c7511085e5640a92ca5e1cf1
kaungsike <kaungsike9@gmail.com>
2026-04-23 17:42:57 +0630

    feat: add profile index screen and settings page navigation

--- 1-startup/app/(tabs)/profile/index.tsx
+++ 1-startup/app/(tabs)/profile/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router';
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 25, fontWeight: "bold", color: "#11181C" }}>Profile</Text>
+      <Link style={{ fontSize: 25, fontWeight: "bold", color: "#11181C" }} href="/(tabs)/profile/setting">Setting</Link>
     </View>
   );
 }
