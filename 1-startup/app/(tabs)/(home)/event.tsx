dfbf93c4709ef2f82435e1c9c9e438a4fb8fff60
kaungsike <kaungsike9@gmail.com>
2026-04-24 16:52:10 +0630

    feat: add event screen with stack configuration and notification button

--- 1-startup/app/(tabs)/(home)/event.tsx
+++ 1-startup/app/(tabs)/(home)/event.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const Event = () => {
   return (
     <View>
+      <Stack.Screen options={{
+        title: "Event",
+        headerRight: () => <Button title='Notification'/>
+      }}/>
       <Text>event</Text>
     </View>
   )
