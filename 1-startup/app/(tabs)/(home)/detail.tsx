445ac3514dcdd85b1fda6c6d0a864f6c55b77bcf
kaungsike <kaungsike9@gmail.com>
2026-04-24 16:39:21 +0630

    feat: add event and detail screens with navigation examples in the home tab

--- 1-startup/app/(tabs)/(home)/detail.tsx
+++ 1-startup/app/(tabs)/(home)/detail.tsx
@@ -1,14 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { Link, useLocalSearchParams, useRouter } from 'expo-router'
 
 const detail = () => {
 
     const {id} = useLocalSearchParams();
 
+    const router = useRouter();
+
   return (
     <View>
       <Text>detail {id}</Text>
+      <Link style={{ fontSize: 25, fontWeight: "bold", color: "#11181C" }} href="/event">Navigate to Event</Link>
+      <Link style={{ fontSize: 25, fontWeight: "bold", color: "#11181C" }} href="/event" replace>Replace with Event</Link>
+      <Button title='Navigate to Event' onPress={ () => router.navigate('/event')}/>
     </View>
   )
 }
---
+++ 1-startup/app/(tabs)/(home)/event.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Event = () => {
+  return (
+    <View>
+      <Text>event</Text>
+    </View>
+  )
+}
+
+export default Event
